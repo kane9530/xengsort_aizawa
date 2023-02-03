5acfbb840b0d87dd24db20ebe71b24d2f455d23f
Kane Toh <kane9530@hotmail.com>
2023-02-03 09:07:49 +0800

    Update 04.star.genomegenerate.sh

--- scripts/04.star.genomegenerate.sh
+++ scripts/04.star.genomegenerate.sh
@@ -2,6 +2,6 @@
 
 STAR --runThreadN 12 \
 	--runMode genomeGenerate \
-	--genomeDir star_index_hg38 star_index_grch38 \
+	--genomeDir star_index_hg38 \
 	--genomeFastaFiles grch38.primary_assembly.fa \
 	--sjdbGTFfile gencode.v42.primary_assembly.annotation.gtf 
